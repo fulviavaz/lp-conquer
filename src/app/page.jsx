70d9d8e41469a75e6e045912426658055c645f99
fulviavaz <fulviavaz.dev@gmail.com>
2024-09-07 11:20:47 -0300

    style: adjustments on main image

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -11,15 +11,19 @@ export default function Home() {
         <Image src="/itau.png" alt="logo itau" width={44} height={44} />
         <Image src="/conquer.png" alt="logo conquer" width={136} height={42}/>
       </nav>
-
-      <div className=" w-full flex justify-center relative items-center mt-10 mb-10">        
-          <Image src="/main.png" width={600} height={400} alt="main" className="rounded-md object-cover" />          
-      </div>
+    <div className="flex flex-col items-center justify-center w-full">
+        <div className=" bg-[url('/main.png')] bg-cover bg-center w-[600px] h-[400px] relative items-center mt-10 mb-10 bg-black rounded-3xl">  
+          <div className="absolute top-80 left-7">
+            <h1 className="text-3xl text-white font-bold">Conquer Pós</h1>
+            <h3 className="text-lg text-white font-bold">Aproveite os descontos!</h3>
+            </div>
+        </div>
+        </div>
 
       <div className="px-20 flex flex-col items-start justify-start w-full gap-6">
-        <h2 className="text-2xl text-text-a font-bold">Garanta seu desconto</h2>
+        <h2 className="2xl:text-2xl text-text-a font-bold">Garanta seu desconto</h2>
         <Button className=" bg-yellow-main text-black text-lg font-bold w-80 h-12">Ir para a Conquer</Button>
-        <p className="text-text-a text-xl">São mais de 80 habilidades
+        <p className="text-text-a 2xl:text-2xl font-bold">São mais de 80 habilidades
           disponíveis! Descubra o poder das
           soft e hard skills na sua carreira.</p>
       </div> 
